9ccce52e42dcc1192e88cc81abe33a670d0ce8ce
igaong <igorngcarioca@gmail.com>
2026-06-23 22:11:54 -0300

    36

--- Unity-Biomas/Assets/Scripts/PainelManager.cs
+++ Unity-Biomas/Assets/Scripts/PainelManager.cs
@@ -4,260 +4,61 @@ using UnityEngine.SceneManagement;
 
 public class PainelManager : MonoBehaviour
 {
+    [Header("PAINEL: Painel 1")]
+    public GameObject painelUm;    // Arraste o painel de campo vazio aqui
 
-    [Header("PAINEL: Açaí")]
-    public GameObject painelAcai;    // Arraste o painel de campo vazio aqui
+    [Header("PAINEL: Painel 2")]
+    public GameObject painelDois;    // Arraste o painel de campo vazio aqui
 
-    [Header("PAINEL: Guaraná")]
-    public GameObject painelGuarana;    // Arraste o painel de campo vazio aqui
+    [Header("PAINEL: Painel 3")]
+    public GameObject painelTres;    // Arraste o painel de campo vazio aqui
 
-    [Header("PAINEL: Vitória-Régia")]
-    public GameObject painelVRegia;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Búgio")]
-    public GameObject painelBugio;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Boto")]
-    public GameObject painelBoto;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Caititu")]
-    public GameObject painelCaititu;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Barauna")]
-    public GameObject painelBarauna;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Cacto")]
-    public GameObject painelCacto;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Juazeiro")]
-    public GameObject painelJuazeiro;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Tatu")]
-    public GameObject painelTatu;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Ararinha")]
-    public GameObject painelArarinha;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Ave")]
-    public GameObject painelAve;    // Arraste o painel de campo vazio aqui
+    [Header("PAINEL: Painel 4")]
+    public GameObject painelQuatro;    // Arraste o painel de campo vazio aqui
 
 
-    [Header("PAINEL: Lobo")]
-    public GameObject painelLobo;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Anta")]
-    public GameObject painelAnta;    // Arraste o painel de campo vazio aqui
-
-    [Header("PAINEL: Tamanduá")]
-    public GameObject painelTamandua;    // Arraste o painel de campo vazio aqui
-
-
-    [Header("PAINEL: Gavião")]
-    public GameObject painelGaviao;    // Arraste o painel de campo vazio aqui
-
-
-
-
-    public void AbrirPainelAcai() {
-        if (painelAcai != null) {
-            painelAcai.SetActive(true);
+    public void AbrirPainelUm() {
+        if (painelUm != null) {
+            painelUm.SetActive(true);
         } else {
         }
     }
-    public void FecharPainelAcai() {
-        if (painelAcai != null) {
-            painelAcai.SetActive(false);
+    public void FecharPainelUm() {
+        if (painelUm != null) {
+            painelUm.SetActive(false);
         }
     }
-
-
-    public void AbrirPainelGuarana() {
-        if (painelGuarana != null) {
-            painelGuarana.SetActive(true);
+    public void AbrirPainelDois() {
+        if (painelDois != null) {
+            painelDois.SetActive(true);
         } else {
         }
     }
-    public void FecharPainelGuarana() {
-        if (painelGuarana != null) {
-            painelGuarana.SetActive(false);
+    public void FecharPainelDois() {
+        if (painelDois != null) {
+            painelDois.SetActive(false);
         }
     }
-
-
-    public void AbrirPainelVRegia() {
-        if (painelVRegia != null) {
-            painelVRegia.SetActive(true);
+    public void AbrirPainelTres() {
+        if (painelTres != null) {
+            painelTres.SetActive(true);
         } else {
         }
     }
-    public void FecharPainelVRegia() {
-        if (painelVRegia != null) {
-            painelVRegia.SetActive(false);
+    public void FecharPainelTres() {
+        if (painelTres != null) {
+            painelTres.SetActive(false);
         }
     }
-
-
-    public void AbrirPainelBugio() {
-        if (painelBugio != null) {
-            painelBugio.SetActive(true);
+    public void AbrirPainelQuatro() {
+        if (painelQuatro != null) {
+            painelQuatro.SetActive(true);
         } else {
         }
     }
-    public void FecharPainelBugio() {
-        if (painelBugio != null) {
-            painelBugio.SetActive(false);
-        }
-    }
-
-
-    public void AbrirPainelBoto() {
-        if (painelBoto != null) {
-            painelBoto.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelBoto() {
-        if (painelBoto != null) {
-            painelBoto.SetActive(false);
-        }
-    }
-    
-
-    public void AbrirPainelCaititu() {
-        if (painelCaititu != null) {
-            painelCaititu.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelCaititu() {
-        if (painelCaititu != null) {
-            painelCaititu.SetActive(false);
-        }
-    }
-
-
-    public void AbrirPainelJuazeiro() {
-        if (painelJuazeiro != null) {
-            painelJuazeiro.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelJuazeiro() {
-        if (painelJuazeiro != null) {
-            painelJuazeiro.SetActive(false);
-        }
-    }
-
-
-    public void AbrirPainelCacto() {
-        if (painelCacto != null) {
-            painelCacto.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelCacto() {
-        if (painelCacto != null) {
-            painelCacto.SetActive(false);
-        }
-    }
-    
-
-    public void AbrirPainelBarauna() {
-        if (painelBarauna != null) {
-            painelBarauna.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelBarauna() {
-        if (painelBarauna != null) {
-            painelBarauna.SetActive(false);
-        }
-    }
-
-
-    public void AbrirPainelTatu() {
-        if (painelTatu != null) {
-            painelTatu.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelTatu() {
-        if (painelTatu != null) {
-            painelTatu.SetActive(false);
-        }
-    }
-
-
-    public void AbrirPainelArarinha() {
-        if (painelArarinha != null) {
-            painelArarinha.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelArarinha() {
-        if (painelArarinha != null) {
-            painelArarinha.SetActive(false);
-        }
-    }
-    
-
-    public void AbrirPainelAve() {
-        if (painelAve != null) {
-            painelAve.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelAve() {
-        if (painelAve != null) {
-            painelAve.SetActive(false);
-        }
-    }
-
-    public void AbrirPainelLobo() {
-        if (painelLobo != null) {
-            painelLobo.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelLobo() {
-        if (painelLobo != null) {
-            painelLobo.SetActive(false);
-        }
-    }
-
-    public void AbrirPainelAnta() {
-        if (painelAnta != null) {
-            painelAnta.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelAnta() {
-        if (painelAnta != null) {
-            painelAnta.SetActive(false);
-        }
-    }
-
-    public void AbrirPainelTamandua() {
-        if (painelTamandua != null) {
-            painelTamandua.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelTamandua() {
-        if (painelTamandua != null) {
-            painelTamandua.SetActive(false);
-        }
-    }
-
-    public void AbrirPainelGaviao() {
-        if (painelGaviao != null) {
-            painelGaviao.SetActive(true);
-        } else {
-        }
-    }
-    public void FecharPainelGaviao() {
-        if (painelGaviao != null) {
-            painelGaviao.SetActive(false);
+    public void FecharPainelQuatro() {
+        if (painelQuatro != null) {
+            painelQuatro.SetActive(false);
         }
     }
 }
